441303d9eedf2acc46f6276be3d650eee360ef84
premakr <premakr86@gmail.com>
2019-07-23 11:07:57 -0700

    commit

---
+++ src/wrapper/A.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+public abstract class A {
+	static void test() {
+		System.out.println("hi");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("from main");
+test();
+	}
+
+}
